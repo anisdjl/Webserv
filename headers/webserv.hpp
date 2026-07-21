05e5c358991108d389c5820eb5088b26c8c86e51
anisdjl <adjelili954@gmail.com>
2026-07-21 18:10:06 +0200

    initial commit

---
+++ headers/webserv.hpp
@@ -0,0 +1,7 @@
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+# include <iomanip>
+# include <iostream>
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+# include "headers/webserv.hpp"
+
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	std::cout << "This is the beggining of Webserv" << std::endl;
+
+	return (0);
+}
