59ac2ab3302d435c4318c3d108768541cd55c408
ymoumene <ymoumene@f6r7s3.paris.42.school>
2026-07-28 19:18:58 +0200

    feat : add in the main loop the listen sockets, create the pollfd vector

--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -2,5 +2,21 @@
 
 bool ft_webserv(Config *config)
 {
-	
+	std::vector<int> listeners(config->listen.size());
+	std::vector<struct pollfd> fds;
+	struct pollfd temp;
+	int i = 0;
+
+	while(i < config->listen.size())
+	{
+		
+		if(ft_listener(config->listen[i], temp.fd))
+			return (true);
+		listeners.push_back(temp.fd);
+		temp.events = POLLIN;
+		temp.revents = 0;
+		fds.push_back(temp);
+		i++;
+	}
+	return (false);
 }
