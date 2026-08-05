f75bfe665fab72cce8cee0576f01e6714078fab5
anisdjl <adjelili954@gmail.com>
2026-08-05 15:41:04 +0200

    parsing: doing the request

--- includes/parsing.hpp
+++ includes/parsing.hpp
@@ -6,21 +6,47 @@
 class LocationConfig;
 class ServerConfig;
 
-class Request
+enum RequestState
 {
-	private:
-		std::string							_start_line;
-		std::map<std::string, std::string>	_header;
-		std::string							_body;
-		long								_content_length;
-
-	public:
-		Request(void);
-		Request(const Request &src);
-		Request &operator=(const Request &src);
-		~Request(void);
+	INCOMPLETE,
+	COMPLETE,
+	ERROR
 };
 
+class HttpRequest
+{
+    public:
+        HttpRequest();
+    	~HttpRequest();
+		std::string	                        	getMethod() const;
+		std::string                         	getPath() const;
+		std::string                         	getQueryString() const;
+		std::string                         	getVersion() const;
+		std::string                         	getBody() const;
+		int										getErrorCode() const;
+		std::string								getHeader(std::string key) const;
+		RequestState							getState() const;
+
+		void									setMethod(std::string method);
+		void 									setPath(std::string path);
+		void 									setQueryString(std::string query);
+		void 									setVersion(std::string version);
+		void									setHeader(std::string key, std::string value);
+		void 									setBody(std::string body);
+		void									setError(int code);
+		void									setState(RequestState state);
+
+		void 									resetRequest();
+	private:
+		RequestState							_state;
+		std::string								_method; // POST
+		std::string								_path;	// a coté de post
+		std::string								_query_string; // les parametres possible
+		std::string								_version; // ex : HTTP/1.1
+		std::map<std::string, std::string>		_header; // donc host, user_agent, content-type, content-length, cookie
+		std::string 							_body; 
+		int										_error;
+};
 
 class Config
 {
