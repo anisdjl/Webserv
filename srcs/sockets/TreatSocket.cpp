697f714ef02724733d05c69704d99a52f4bd77d2
ymoumene <ymoumene@student.42.fr>
2026-08-07 15:55:11 +0200

    fix: add includes in TreatSocket

--- srcs/sockets/TreatSocket.cpp
+++ srcs/sockets/TreatSocket.cpp
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "../../includes/socket/Socket.hpp"
+#include "../../includes/socket/Connection.hpp"
+#include "../../includes/socket/Cgi.hpp"
 
 bool ft_parse_request(std::map<int, Socket> &map_socket, Connection &target, Config *config, const int &epollfd)
 {
