c5636958597a297f147722f3687ede8b4775a53e
ymoumene <ymoumene@student.42.fr>
2026-08-07 16:52:53 +0200

    fix: setter and getter moved in hpp for Socket class and their child

--- includes/socket/Cgi.hpp
+++ includes/socket/Cgi.hpp
@@ -6,12 +6,13 @@
 /*   By: ymoumene <ymoumene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 20:02:41 by ymoumene          #+#    #+#             */
-/*   Updated: 2026/08/07 14:55:01 by ymoumene         ###   ########.fr       */
+/*   Updated: 2026/08/07 16:50:11 by ymoumene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGI_HPP
 #define CGI_HPP
+
 
 #include "Socket.hpp"
 
@@ -20,13 +21,40 @@ class Cgi : public Socket
     private :
             int 	   		_parent_index;
     public:
-        int getParentIndex() const;
-        void setParentIndex(int index);
-        Cgi();
-        Cgi(int fd, int server_index, int parent_index);
-        Cgi(const Cgi& other);
-        ~Cgi();
-        Cgi& operator=(const Cgi& other);
+        int getParentIndex() const
+		{
+			return (this->_parent_index);
+		};
+        void setParentIndex(int index)
+		{
+			this->_parent_index = index;
+		};
+        Cgi() : Socket(), _parent_index(-1)
+		{
+			this->_type = CGI;
+		};
+        Cgi(int fd, int server_index, int parent_index) 
+		: Socket(fd, server_index, CGI), _parent_index(parent_index)
+		{};
+		Cgi(const Cgi& src) : Socket(src), _parent_index(src._parent_index)
+		{
+			
+		};
+        ~Cgi()
+		{
+
+		};
+        Cgi& operator=(const Cgi& src)
+		{
+			if (this != &src)
+			{
+				this->_fd = src._fd;
+				this->_server_index = src._server_index;
+				this->_type = src._type;
+				this->_parent_index = src._parent_index;
+			}
+			return (*this);
+		};
 };
 
 #endif
--- includes/socket/Socket.hpp
+++ includes/socket/Socket.hpp
@@ -6,7 +6,7 @@
 /*   By: ymoumene <ymoumene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/01 17:39:24 by ymoumene          #+#    #+#             */
-/*   Updated: 2026/08/07 15:39:00 by ymoumene         ###   ########.fr       */
+/*   Updated: 2026/08/07 16:05:17 by ymoumene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,23 +32,58 @@ class Socket
     	int         	_server_index;
     	Socket_type 	_type;
 		
-		Socket();
-		Socket(int fd, int server_index, Socket_type type);
-		Socket(const Socket& src);
+		Socket() : _fd(-1), _server_index(-1), _type(LISTENER) {};
+		Socket(int fd, int server_index, Socket_type type)
+		: _fd(fd), _server_index(server_index), _type(type) 
+		{};
+		Socket(const Socket& src) 
+		: _fd(src._fd), _server_index(src._server_index), _type(src._type) 
+		{};
 		
 	public :
 
 		
 		~Socket();
-		int &getFd();
-		int getFd() const;
-		int getServerIndex() const;
-		Socket_type getType() const;
-		void setFd(int fd);
-		void setServerIndex(int index);
-		void setType(Socket_type type);
-		Socket& operator=(const Socket& src);
+		int &getFd()
+		{
+			return (this->_fd);
+		};
+		int getFd() const
+		{
+			return (this->_fd);
+		};
+		int getServerIndex() const
+		{
+			return (this->_server_index);
+		};
+		Socket_type getType() const
+		{
+			return (this->_type);
+		};
+		void setFd(int fd)
+		{
+			this->_fd = fd;
+		};
+		void setServerIndex(int index)
+		{
+			this->_server_index = index;
+		};
+		void setType(Socket_type type)
+		{
+			this->_type = type;
+		};
+		Socket& operator=(const Socket& src)
+		{
+			if (this != &src)
+			{
+				this->_fd = src._fd;
+				this->_server_index = src._server_index;
+				this->_type = src._type;
+			}
+			return (*this);
+		};
 };
+
 
 bool ft_treat_socket(std::map<int, Socket> &map_socket, struct epoll_event &event, Config *config, const int &epollfd);
 bool ft_construct_listener(std::map <int, Socket> &map_socket, Config *config, int const &epollfd);
